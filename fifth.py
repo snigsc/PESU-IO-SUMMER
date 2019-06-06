c098a9ab45412a90431be6b5128e6558026c34f3
snigsc <51334429+snigsc@users.noreply.github.com>
2019-06-06 21:46:37 +0530

    Delete fifth.py

--- fifth.py
+++
@@ -1,9 +0,0 @@
-string = input('Enter string : ')
-count = 0
-for i in string :
-    if '0' <= i <= '9' :
-        count+=1
-if count == len(string) :
-    print('Numeric string')
-else :
-    print('Not numeric string')
